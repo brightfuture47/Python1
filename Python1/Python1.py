9a61bbc4eed38d6f63d8bb4f94fa8f0c483de9d3
future <twixxx2457@gmail.com>
2022-01-28 01:17:11 +0300

    Урок 2 часть 2, Арифметика

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,23 +1,41 @@
-#название переменной = значение 
-name = "Future"
-Name = "Future"
-NAME = "Future"
-# типы данных:
-# boolean - true false
-# int - 2 5 34
-# float - 1.5 2.34
-# complex
-# str "Future"
-# bytes 0 - 255
-# list
-# turple
-# set 
-# dict
-#x = 3.8e3 #3.8 * 10^3
-#print(x)
-#x = 3.8e-3 #3.8 * 10^(-3)
-#print(x)
-a = "123RTY1234"
-print(type(a))
-a = 123
-print(type(a))
+print(3+3)
+
+print(23-13)
+
+print(6*8)
+
+print(7/2)
+
+print(7//2)
+
+#возведение в степень
+print(2**2)
+
+#Вычисление корня
+print(4 ** 0.5)
+
+#Остаток от деления
+print(7%2)
+
+# приоритет арифмитических операций
+# ** | * / // % | + -
+number = 2+4 - 3*2 + 2**2
+print(number)
+number = (2+4) - (3*2 + 2**2)
+print(number)
+
+#Погрешность при вычислении операции с точкой
+print(1/3 + 1/3 + 1/3)
+
+# += | -= | *= | /= | //= | **= | %= |
+number = 15
+number += 5 #number = number + 5
+print (number)
+
+#функции преобразования чисел
+number1 = "5"
+number2 = 5
+sum = int(number1) + number2
+print (sum)
+
+a= int(input())
