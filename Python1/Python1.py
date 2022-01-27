76f159ec9a58a4aa1098d99a82203cb88b4114d0
future <twixxx2457@gmail.com>
2022-01-28 00:58:26 +0300

    2 урок 1 часть

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,7 +1,23 @@
-name = input()
-print("Hello", name)
-print(2+10) #вывод суммы двух чисел
-print("Hello")
-if 1 < 10:
-    print("1<10")
-print("After") 
+#название переменной = значение 
+name = "Future"
+Name = "Future"
+NAME = "Future"
+# типы данных:
+# boolean - true false
+# int - 2 5 34
+# float - 1.5 2.34
+# complex
+# str "Future"
+# bytes 0 - 255
+# list
+# turple
+# set 
+# dict
+#x = 3.8e3 #3.8 * 10^3
+#print(x)
+#x = 3.8e-3 #3.8 * 10^(-3)
+#print(x)
+a = "123RTY1234"
+print(type(a))
+a = 123
+print(type(a))
