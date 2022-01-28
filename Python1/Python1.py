cbc452be192a2d69ff20d484848bcb8b3558f7eb
future <twixxx2457@gmail.com>
2022-01-28 17:29:55 +0300

    делится ли число на 6

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,13 +1,4 @@
-﻿print("Введите номер месяца, чтобы узнать время года: ")
-a = int(input())
+﻿a = int(input())
 
-if a == 12 or a == 1 or a == 2:
-    print("Зима")
-elif a == 3 or a == 4 or a == 5:
-    print("Весна")
-elif a == 6 or a == 7 or a == 8:
-    print("Лето")
-elif a == 9 or a == 10 or a == 11:
-    print("Осень")
-else:
-    print("месяцев в году 12!!!")
+if a % 6 == 0:
+    print("Okay")
