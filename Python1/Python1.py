4a2f942b7898743e0b80ba4570028bea0f9602ae
future <twixxx2457@gmail.com>
2022-01-28 15:22:40 +0300

    Логические операции

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,13 +1,26 @@
-#Сравнения
-# == 
-# !=
-# >
-# <
-# >=
-# <=
-a = 3
-b = 7
-c = a == b
-print(c)
-c = a != b
-print(c)
+# логические операции
+# and:
+# True and True -> True
+# False and true -> False
+a = 23
+b = 56
+c = a > 10 and b <100
+#print(c) ->true
+
+# or 
+# True or True -> True
+# True or False -> True
+# False or False -> False
+a = 24
+b = 24
+c = a == b or a>100 or b<50
+#print(c) -> True
+
+#not
+#not True -> False
+#not False -> True
+a = 23
+c = a>10
+# print (c) -> True
+c = not a > 10
+# print (c) -> False
