c2b853461626ebd1140b96c8dd5b04b521b4f23e
future <twixxx2457@gmail.com>
2022-01-29 20:50:32 +0300

    Любовный калькулятор

--- Python1/Python1.py
+++ Python1/Python1.py
@@ -1,4 +1,35 @@
-﻿a = int(input())
+﻿from tkinter import *
+from tkinter.messagebox import showinfo
+#from PIL import Image, ImageTk
+from random import randint
 
-if a % 6 == 0:
-    print("Okay")
+win = Tk()
+#win.iconbitmap (heart.ico)
+win.title("Гей калькулятор")
+
+def calculation():
+    number = randint(0,100)
+    showinfo("Процент гейства", f"Это гейство на {number} %")
+
+#open_img = Image.open("Love_main.jpg")
+#render_img = ImageTk.PhotoImage(open_img)
+#main_image = Label(win,image=render_img)
+#main_image.image = render_img
+#main_image.grid(row=0,columnspan=2)
+
+label1 = Label(win,text="Введите Ваше имя:",font=("arial",10,"bold"))
+label1.grid(row=1, column=0, sticky="w")
+
+entry1 = Entry(win, font=("arial",10,"bold"))
+entry1.grid(row=1,column=1)
+
+label1 = Label(win,text="Введите имя Вашего партнера:",font=("arial",10,"bold"))
+label1.grid(row=2,column=0)
+
+entry2 = Entry(win, font=("arial",10,"bold"))
+entry2.grid(row=2, column=1)
+
+button = Button(win, text= "Проверить!!", font=("arial",12,"bold"), fg='red',bg='black', command=calculation)
+button. grid(row=3, columnspan=2)
+
+win.mainloop()
